0f334a8c0457079080b3f2f54c1eaf08bbbbefd4
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-08-30 13:41:03 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,17 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    def tarjetas(x, y):
+        tppl=x*12
+        tpp=y*35
+        if tppl >= tpp:
+            return tpp
+        elif tppl <= tpp:
+            return tppl
+    
+    pl=int(input("Dame la cantidad de pliegos de papel albanene: "))
+    p=int(input("Dame la cantidad de plumones: "))
+    print("El número máximo de tarjetas que se pueden hacer es: " + str(tarjetas(pl, p)))
     pass
 
 if __name__=='__main__':
